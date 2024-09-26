265fbf51a9e6475c1b218ecf5bd9edc9cb4e178c
Misho0oka <43311713+Misho0oka@users.noreply.github.com>
2024-09-26 13:38:12 +0300

    Update Program.cs

--- TCP_Client/Program.cs
+++ TCP_Client/Program.cs
@@ -10,24 +10,30 @@ class Client
 
     static void Main()
     {
+        //създаване на клиета ip и порт
         TcpClient client = new TcpClient(ServerIp, Port);
         Console.WriteLine("Connected to server. Start chatting!");
 
         NetworkStream stream = client.GetStream();
-
+        //създава се stream за получаване на информацията
         Thread receiveThread = new Thread(ReceiveMessages);
+        //пускане на thred-a
         receiveThread.Start(stream);
-
+        // цикъл за кодиране
         while (true)
         {
+            //вземане на текст от конзолата
             string message = Console.ReadLine();
+            //масив от байтове
             byte[] buffer = Encoding.ASCII.GetBytes(message);
+            //изпращане
             stream.Write(buffer, 0, buffer.Length);
         }
     }
-
+ //метод за съобщенията
     static void ReceiveMessages(object obj)
     {
+        
         NetworkStream stream = (NetworkStream)obj;
         byte[] buffer = new byte[1024];
         int bytesRead;
@@ -36,15 +42,18 @@ class Client
         {
             try
             {
+                //вземане на съобщение
                 bytesRead = stream.Read(buffer, 0, buffer.Length);
+                //проверяване дали има някой
                 if (bytesRead == 0)
                 {
                     break;
                 }
-
+                //стринг за съобщението
                 string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
                 Console.WriteLine(message);
             }
+            //проверка за грешка
             catch (Exception)
             {
                 break;
